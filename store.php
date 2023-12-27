68c5f3de79582b85ac92f28b23a7c753b255a360
joshua496 <130254515+joshua496@users.noreply.github.com>
2023-12-27 04:43:37 +0000

    edit

--- store.php
+++ store.php
@@ -1,12 +1,13 @@
 <html>
 <body><h1>
     <?php
-    $quantity= $_POST['cquan'];
-    $payment= $_POST['cpay'];
-    $selectedProduct =$_POST['coffee'];
+    
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     foreach($selectedProduct as $price){
         $selectedProduct = $_POST['coffees'];
+        $quantity= $_POST['cquan'];
+        $payment= $_POST['cpay'];
+        $selectedProduct =$_POST['coffee'];
         echo "Your Product is: " . $selectedProduct;
         $bill=$quantity*$price;
         echo "Your Product is: " . $bill;
