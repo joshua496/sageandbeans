87abd095797fe182cefcc12f2dcbfcceb53666a3
joshua496 <130254515+joshua496@users.noreply.github.com>
2023-12-27 04:34:40 +0000

    edit php

--- store.php
+++ store.php
@@ -1,11 +1,14 @@
 <html>
 <body><h1>
     <?php
-
+    $quantity= $_POST['cquan'];
+    $payment= $_POST['cpay'];
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if(isset($_POST['coffees'])){
         $selectedProduct = $_POST['coffees'];
         echo "Your Product is: " . $selectedProduct;
+        $bill=$quantity*$selectedProduct;
+        echo "Your Product is: " . $bill;
     }
 }
 ?>
