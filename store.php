0a6315549706437d514a019049b03b2964955461
UrekMazi <123257809+UrekMazi@users.noreply.github.com>
2023-12-28 00:03:50 +0000

    Pending changes exported from your codespace

--- store.php
+++ store.php
@@ -24,38 +24,56 @@ foreach ($values as $price){
     $bill = $price * $quantity;
     $payment = $_POST['cpay'];
     $change= $payment - $bill;
-    
+
     if($bill>1000){
         $status= "Good Sale";
     }
     else{
          $status= "Bad Sale";
     }
-    if ($change<$bill){
-        $change= "Your payment is not enough!";
-    }
-    else {
-        $change= $change;
-    }
-    if ($change<$bill){
-        $status="";
-    }
-    else {
-        $status= $status;
-    }
 }
 ?>
 <div class="card-container">
         
-        <div class="card">
-       <h2>Thank you for Purchasing</h2>
+       <div class="card">
+       <h2><?php
+       if ($payment>$bill){
+        echo "Thank you for your purchase!";
+       }
+       else {
+        echo "Your Payment is not enough!";
+       }
+       ?></h2><?php
+       if ($payment>$bill){
+        echo '<img src="images/cat.jpg">';
+       }
+       else {
+        echo '<img.src="images/mike.jpg">';
+       }
+       ?>
        <img src="images/cat.jpg">
        <!-- insert lang ang bill diri-->
        <p>Your Total Bill is: ₱<?php echo $bill ?></p>
+       <!--insert lang ang payment diri-->
+       <p>Your Cash is: ₱<?php echo $payment ?></p>
        <!-- insert lang ang Change diri-->
-       <p>Your Change is: ₱<?php echo $change ?></p>
+       <p><?php     
+        if ($payment>$bill){
+            echo "Your Change is: ₱".$change.'';
+        }
+        else{
+            echo "";
+        }
+       ?></p>
         <!-- insert lang ang status diri-->
-       <p>Sale Status: <?php echo $status ?></p>
+       <p><?php
+        if ($payment>$bill){
+            echo "Sale Status: ".$status.'';
+        }
+        else {
+            echo "";
+        }
+       ?></p>
        <p><a href="store.html">Back To The Store</a></p>
         </div>
         
